72b41a284f93c29713818ba5ffe5151f07650b83
xanderyanov <xanderyanov@gmail.com>
2020-11-06 12:39:14 +0400

    12-39

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,11 +5,21 @@
 $(function () {
   $(".go_to1").on("click", function () {
     var scroll_el = $(".go_adr1");
-    headerHeight = $(".header__area").height();
+    // headerHeight = $(".header__area").height();
     if ($(scroll_el).length != 0) {
-      $("html, body").animate({ scrollTop: $(scroll_el).offset().top - headerHeight }, 1000);
+      $("html, body").animate({ scrollTop: $(scroll_el).offset().top /*- headerHeight*/ }, 1000);
     }
     return false;
+  });
+
+  $window = $(window);
+  $('div[data-type="background"]').each(function () {
+    var $bgobj = $(this);
+    $(window).on("scroll", function () {
+      var yPos = -($window.scrollTop() / $bgobj.data("speed"));
+      var coords = "50% " + yPos + "px";
+      $bgobj.css({ backgroundPosition: coords });
+    });
   });
 
   if ($(".carousel__container").length) {
